568c19a2165892432f4005c3764c9dd13027d308
Alihan Kuru <1800002457@STU.IKU.EDU.TR>
2023-02-11 01:03:18 +0300

    Add project files.

---
+++ Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+            => new() {
+                new(){Id=Guid.NewGuid(), Name="Product 1", CreatedDate=DateTime.Now , Price= 10, Stock=5},
+                new(){Id=Guid.NewGuid(), Name="Product 2", CreatedDate=DateTime.Now , Price= 10, Stock=5},
+                new(){Id=Guid.NewGuid(), Name="Product 3", CreatedDate=DateTime.Now , Price= 10, Stock=5},
+                new(){Id=Guid.NewGuid(), Name="Product 4", CreatedDate=DateTime.Now , Price= 10, Stock=5},
+                new(){Id=Guid.NewGuid(), Name="Product 5", CreatedDate=DateTime.Now , Price= 10, Stock=5}
+            };
+    }
+}
