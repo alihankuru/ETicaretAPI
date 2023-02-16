eff3a122a2ab162e5798fccff0aad67cab9a7349
Alihan Kuru <1800002457@STU.IKU.EDU.TR>
2023-02-16 23:09:33 +0300

    Tracking eklendi eğer (,false) yazılırsa bilinçli bir şekilde etkisiz bırakıyor.

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using ETicaretAPI.Application.Abstractions;
 using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,17 +20,33 @@ namespace ETicaretAPI.API.Controllers
         }
 
         [HttpGet]
-       public async void Get()
+       public async Task Get()
         {
-           await _productWriteRepository.AddRangeAsync(new()
-            {
+            //await _productWriteRepository.AddRangeAsync(new()
+            // {
+            //     new(){Id=Guid.NewGuid(), Name="AK1", CreatedDate=DateTime.UtcNow, Price=10, Stock=100},
+            //     new(){Id=Guid.NewGuid(), Name="AK2", CreatedDate=DateTime.UtcNow, Price=20, Stock=100},
+            //     new(){Id=Guid.NewGuid(), Name="AK3", CreatedDate=DateTime.UtcNow, Price=30, Stock=100},
 
-                new(){Id=Guid.NewGuid(), Name="AK1", CreatedDate=DateTime.UtcNow, Price=10, Stock=100},
-                new(){Id=Guid.NewGuid(), Name="AK2", CreatedDate=DateTime.UtcNow, Price=20, Stock=100},
-                new(){Id=Guid.NewGuid(), Name="AK3", CreatedDate=DateTime.UtcNow, Price=30, Stock=100},
-            });
-            var count = await _productWriteRepository.SaveAsync();
+            // });
+            // var count = await _productWriteRepository.SaveAsync();
+
+
+            Product p = await _productReadRepository.GetByIdAsync("33d7e02a-72bc-4109-8ebb-97cd46fbadea",false);
+            p.Name = "Mehmet";
+            await _productWriteRepository.SaveAsync();
         }
-       
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get (string id)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(id);
+
+            return Ok(product);
+        }
+
+
+
     }
 }
